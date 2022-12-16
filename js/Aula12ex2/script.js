9aaff4b93aceb4889fb5bba8a9497cd47f4a58e9
joao-vitor-viera-rodacki <cafepretolok@gmail.com>
2022-12-15 21:53:28 -0300

    add new exerc

--- js/Aula12ex2/script.js
+++ js/Aula12ex2/script.js
@@ -6,31 +6,77 @@ var img = window.document.getElementById("img")
 var data = new Date()
 var ano = data.getFullYear()
 
-but.addEventListener("click", verifica_sexo)
+but.addEventListener("click", verifica_idade)
 
 // console.log(idade.value)
 // console.log(sex)
 // console.log(sex[1].checked)
-// console.log(sex[0].checked)
+// console.log(sex[0].checked
+// img.setAttribute("src","endereco")
+// res.appendChild(img)
 
-console.log(ano)
-function verifica_sexo(){
-    idade_ano = idade_ano.value
-    console.log(idade_ano)
-    if (sex[0].checked == true){
-        var idade = verifica_idade(idade_ano)
-        res.innerHTML = `<p>homem idade ${idade}</p>`
-        img.src = "img/homem.jpg"
-        return
+function verifica_entrada(size ,value){
+    value = Number(value)
+    if (size < 4 || value == NaN || value > ano){
+        return false
+    }else {
+        return true
+    }
 
-    }else if(sex[1].checked == true){
-        img.src = "img/mulher.jpg"
-        console.log("mulher")
+}
+function verifica_idade(){
+    ida = idade_ano.value
+    size = idade_ano.value.length
+    console.log(ida,size)
+    veri = verifica_entrada(size,ida)
+
+    if(veri){
+        if (sex[0].checked == true){
+            
+            var idade = verifica_sexo(ida,"homem")
+            
+            res.innerHTML = `<p>homem idade ${idade}</p>`
+        
+
+        }else if(sex[1].checked == true){
+            var idade = verifica_sexo(ida,"mulher")
+            res.innerHTML = `<p>Mulher ; idade ${idade}</p>`
+            
+        }
+    }else{
+        return alert("digite novamente")
     }}
 
-function verifica_idade(ida_ano){
-    console.log(ida_ano)
-
+function verifica_sexo(ida_ano,sexo){
     idade =   ano - ida_ano
-    return idade
+    console.log(idade)
+    if (idade <= 12 && idade > 0){
+        if (sexo == "homem"){
+            img.src = "img/menino.jpg"
+            return idade
+        }else{
+            img.src = "img/menina.jpg"
+            return idade
+        }
+
+    }else if(idade > 12 && idade <= 50){
+        if (sexo == "homem"){
+            img.src = "img/homem.jpg"
+            return idade
+
+        }else{
+            img.src = "img/mulher.jpg"
+            return idade
+
+        }
+    }else if(idade > 50 && idade > 100){
+        if (sexo == "homem"){
+            img.src = "img/velho.jpg"
+            return idade
+
+        }else{
+            img.src = "img/velha.jpg"
+            return idade
+        }
+    }
 }
