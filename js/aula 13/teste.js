6d2fec6d17309f56be8acd3e346b35a66477cdaf
joao-vitor-viera-rodacki <cafepretolok@gmail.com>
2023-01-03 21:02:33 -0300

    Add new exercices

---
+++ js/aula 13/teste.js
@@ -0,0 +1,15 @@
+// while(condicao){
+    // codigo
+// }
+
+// do{
+    // codigo
+// }while(condicao)
+c = 10
+while(c != 0){
+    console.log(`Agora ${c}`)
+    c--
+    // if(c == 5){
+    //     console.log(`metade ${c}`)
+    // }
+}
