03793c2e3f45d949bd9540f561e8a376cba407e8
joao-vitor-viera-rodacki <cafepretolok@gmail.com>
2023-02-12 19:03:40 -0300

    I lear about tag select in html

--- js/Aula 13/ex02/main.js
+++ js/Aula 13/ex02/main.js
@@ -1,10 +1,20 @@
 var number = window.document.getElementById("numero")
 var tell = window.document.getElementById("contar")
-var numero = Number(number.value)
-
+var select = window.document.getElementById("select")
 tell.addEventListener("click",tellFunction)
 
 function tellFunction(){
-    return numero
+    number2 = Number(number.value)
+    if(number.value.length == 0 ){
+        window.alert("Digite um numero")
+    }else{
+        select.innerHTML = ``
+        for(cont = 0;cont <= 10; cont++){
+            let item = document.createElement('option')
+            item.text = `${number2} x ${cont} = ${number2*cont}`
+            item.value = `tab${cont}`
+            select.appendChild(item)
+
+        }
+    }
 }    
-export default tellFunction;
